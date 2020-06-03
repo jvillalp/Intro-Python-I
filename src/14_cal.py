37df450e837d067c0fbb102400cdcf07c07e53b5
Juana Villalpando <juana.v14@gmail.com>
2020-06-02 23:08:56 -0700

    14 complete

--- src/14_cal.py
+++ src/14_cal.py
@@ -34,20 +34,18 @@ from datetime import datetime
 l = len(sys.argv)
 
 if l ==1:
-    month = datetime.now().month
-    year = datetime.now().year
+  month = datetime.now().month
+  year = datetime.now().year
 if l == 2:
-     month = int(sys.argv[1])
-     year = datetime.now().year
+  month = int(sys.argv[1])
+  year = datetime.now().year
 if l ==3:
-     month = int(sys.argv[1])
-     year = int(sys.argv[2])
+  month = int(sys.argv[1])
+  year = int(sys.argv[2])
 else:
-    print("usage: calander.py [month] [year]")
-    sys.exit(1)
+  print("usage: calander.py [month] [year]")
+  sys.exit(1)
 
 cal = calendar.TextCalendar()
 
 cal.prmonth(year, month)
-
-# print(month, year)
